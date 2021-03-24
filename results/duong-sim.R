699eef53c8454da9abd876d687873b1702a219cc
Marlena Bannick <mnorwood@uw.edu>
2021-03-24 12:15:42 -0700

    finished with duong simulation

--- results/duong-sim.R
+++ results/duong-sim.R
@@ -42,10 +42,13 @@ process.study <- function(file){
   return(df)
 }
 
-fit.model <- function(df){
+fit.model <- function(df, knot=5){
+
+  # Indicator variable for sample 5+
+  df[, samp.5 := samp >= knot]
 
   # Fit GEE model for the gap times
-  mod <- gee(gap ~ 1, id=id.key, data=df,
+  mod <- gee(gap ~ samp + samp.5 + samp.5*samp, id=id.key, data=df,
              family=poisson(link="log"), corstr="exchangeable")
 
   # Get only the first days of sampling
@@ -53,22 +56,28 @@ fit.model <- function(df){
 
   return(list(days=first.day$days,
               num.samples=first.day$num.samples,
-              rate=exp(coef(mod))))
+              rate=coef(mod)))
 }
 
-simulate.study <- function(days, num.samples, rate){
-
+simulate.study <- function(days, num.samples, coefs, knot=5){
   # Sample the first day
   day1 <- sample(days, size=length(days), replace=TRUE)
 
   # Sample the number of samples
   nums <- sample(num.samples, size=length(days), replace=TRUE)
 
-  # Get the rate for gap times
-  # rate <- cbind(rep(1, length(days)), day1, nums) %*% coef(mod)
+  # Get a vector of the sample numbers, starting from the second one
+  samp.num <- lapply(nums, function(x) 2:x)
+
+  # Get a sample design matrix
+  get.samp.dmat <- function(samp) return(cbind(1, samp, samp>=knot, (samp>=knot)*samp))
+
+  dmats <- lapply(samp.num, get.samp.dmat)
+
+  rates <- lapply(dmats, function(x) exp(x %*% coefs)[, 1])
 
   # Get the gap-times for each
-  gaps <- lapply(nums, function(x) rpois(n=x, lambda=rate))
+  gaps <- lapply(rates, function(x) rpois(n=length(x), lambda=x))
 
   # Add on the start day
   days.gap <- mapply(function(x, y) c(x, y), x=day1, y=gaps)
@@ -97,82 +106,20 @@ simulate.studies <- function(file, nsims){
 # NUMBER OF SAMPLES TAKEN PER PANEL ----------------------------------
 # --------------------------------------------------------------------
 
+# Raw Data
 df <- process.study(file=FILE)
 num.samples <- df[, lapply(.SD, max), .SDcols="samp", by="id.key"]
 samps <- num.samples$samp
-pdf("duong-hist-data.pdf", width=8, height=4)
+
+set.seed(365)
+# Simulation
+sims <- simulate.studies(file=FILE, nsims=3)
+
+pdf("duong-hist-fig.pdf", width=8, height=5)
 par(mfrow=c(1, 2))
-hist(samps, main="Histogram of Number of Samples",
-     xlab="Number of Samples Taken")
-hist(df$days, main="Days Post Seroconversion Histogram",
-     xlab="Days")
+hist(df$days, main="Days Post Seroconversion \n(Observed)",
+     xlab="Days", breaks=50, ylim=c(0, 205))
+hist(sims[[1]]$days, main="Days Post Seroconversion \n(One Simulation)",
+     xlab="Days", breaks=50, col='lightblue',
+     ylim=c(0, 205))
 dev.off()
-
-sims <- simulate.studies(file=FILE, nsims=100)
-
-par(mfrow=c(2, 2))
-hist(df$days)
-hist(sims[[1]]$days)
-hist(sims[[2]]$days)
-hist(sims[[3]]$days)
-
-# Fit GEE model for the gap times
-df[, samp.5 := samp >= 5]
-mod <- gee(gap ~ samp + samp.5 + samp.5*samp, id=id.key, data=df,
-           family=poisson(link="log"), corstr="exchangeable")
-
-dd <- data.table(samp=1:25)
-dd[, samp.5 := samp >= 5]
-
-int <- rep(1, 25)
-samp <- 1:25
-samp.5 <- samp >= 5
-
-dd <- cbind(int, samp, samp.5, samp*samp.5)
-preds <- exp(dd %*% coef(mod))
-dd <- data.table(dd)
-dd[, preds := preds]
-
-ggplot(data=df) + geom_line(aes(x=samp, y=gap, group=id.key)) +
-  geom_line(data=dd, aes(x=samp, y=preds), color='red')
-
-
-# TODO: Put this into the function.
-first.day <- df[samp==1]
-days <- first.day$days
-num.samples <- first.day$num.samples
-# Sample the first day
-day1 <- sample(days, size=length(days), replace=TRUE)
-
-# Sample the number of samples
-nums <- sample(num.samples, size=length(days), replace=TRUE)
-
-# Get sample numbers
-samp.num <- lapply(nums, function(x) 1:x)
-
-get.samp.dmat <- function(samp) return(cbind(1, samp, samp>=5, (samp>=5)*samp))
-
-dmats <- lapply(samp.num, get.samp.dmat)
-
-rates <- lapply(dmats, function(x) exp(x %*% coef(mod))[, 1])
-
-# Get the gap-times for each
-gaps <- lapply(rates, function(x) rpois(n=length(x), lambda=x))
-
-# Add on the start day
-days.gap <- mapply(function(x, y) c(x, y), x=day1, y=gaps)
-
-# Calculate sampling times
-days.tot <- lapply(days.gap, cumsum)
-
-# Get ids and put into data frame
-ids <- 1:length(days)
-ids <- rep(ids, nums)
-
-hist(unlist(days.tot), breaks=50)
-
-
-
-
-hist(df$days, main="Days Post Seroconversion Histogram",
-     xlab="Days", breaks=50)
