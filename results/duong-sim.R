b389e52b197120613acca928abddf4855258e8c2
Marlena Bannick <mnorwood@uw.edu>
2021-03-23 22:01:55 -0700

    adding the duong sim

--- results/duong-sim.R
+++ results/duong-sim.R
@@ -7,156 +7,172 @@ library(lme4)
 library(gridExtra)
 library(magrittr)
 
-# --------------------------------------------------------------------
-# DATA CLEANING ------------------------------------------------------
-# --------------------------------------------------------------------
-
-df <- fread("~/OneDrive/Documents/2020_2021/RA FEI GAO/duong2015.csv")
-df <- df[!is.na(days)]
-
-ids <- df[, .(id1)]
-ids <- unique(ids)
-ids[, id.key := .I]
-
-df <- merge(df, ids, by=c("id1"))
-setorder(df, id.key, days)
-df <- df[, .(id.key, days)]
-df[, samp := 1:.N, by="id.key"]
-df[days == 0, days := 0.5]
-
-hist(df$days, main="Days Post Sero-Conversion Histogram",
-     xlab="Days")
-
-# --------------------------------------------------------------------
-# TRAJECTORIES OF SAMPLING TIMES WITHIN A PANEL ----------------------
-# --------------------------------------------------------------------
-
-# Plots of the days since seroconversion of sampling over the sampling id
-p1 <- ggplot(data=df) + geom_line(aes(x=samp, y=days, group=id.key)) +
-  ggtitle("Trajectories of Days Post\nSero-Conversion by Sample Number") +
-  labs(x="Sample Number", y="Days Post Sero-Conversion")
-p2 <- ggplot(data=df) + geom_line(aes(x=samp, y=log(days), group=id.key)) +
-  ggtitle("Trajectories of Log Days Post\nSero-Conversion by Sample Number") +
-  labs(x="Sample Number", y="Log Days Post Sero-Conversion")
-grid.arrange(p1, p2, nrow=1)
-
-# Make individual trajectories of how many
-# days post seroconversion it took they were sampled at
-df[, samp2 := samp**2]
-df[, samp3 := samp**3]
-mod <- lmer(log(days) ~ samp + samp2 + samp3 + (1|id.key), data=df)
-# mod <- lmer(log(days) ~ samp + (1|id.key), data=df)
-df[, pred := predict(mod)]
-
-pdf("duong-trajectories-data.pdf", width=6, height=6)
-ggplot(data=df) + geom_line(aes(x=samp, y=days, group=id.key)) +
-  # geom_line(aes(x=samp, y=exp(pred), group=id.key), color='red') +
-  ggtitle("Individual Trajectories") +
-  labs(x="Sample Number", y="Days Post Seroconversion")
-dev.off()
-
-# --------------------------------------------------------------------
-# NUMBER OF SAMPLES TAKEN PER PANEL ----------------------------------
-# --------------------------------------------------------------------
-
-num.samples <- df[, lapply(.SD, max), .SDcols="samp", by="id.key"]$samp
-pdf("duong-hist-data.pdf", width=8, height=4)
-par(mfrow=c(1, 2))
-hist(num.samples, main="Histogram of Number of Samples",
-     xlab="Number of Samples Taken")
-hist(df$days, main="Days Post Seroconversion Histogram",
-     xlab="Days")
-dev.off()
+FILE <- "~/OneDrive/Documents/2020_2021/RA FEI GAO/duong2015.csv"
 
 # --------------------------------------------------------------------
 # DATA GENERATING MECHANISM ------------------------------------------
 # --------------------------------------------------------------------
 
-ids <- unique(df$id.key)
+process.study <- function(file){
+  # Read in data
+  df <- fread(file)
 
-make.trajectory <- function(id, n){
-  cos <- coef(mod)$id.key[id, ] %>% t
-  samp.id <- 1:n
-  variables <- cbind(rep(1, n), samp.id, samp.id**2, samp.id**3)
-  # variables <- cbind(rep(1, n), samp.id)
-  days <- exp(variables %*% cos) %>% round
-  return(days)
+  # Data pre-processing
+  df <- df[!is.na(days)]
+
+  ids <- df[, .(id1)]
+  ids <- unique(ids)
+  ids[, id.key := .I]
+
+  df <- merge(df, ids, by=c("id1"))
+  setorder(df, id.key, days)
+  df <- df[, .(id.key, days)]
+  df[, samp := 1:.N, by="id.key"]
+
+  # Calculate gap days between samples
+  df[, last.time := shift(days), by="id.key"]
+  df[, gap := days - last.time]
+
+  # Calculate total number of samples
+  df[, num.samples := .N, by="id.key"]
+
+  # Calculate first day
+  df[, first.samp := lapply(.SD, min), .SDcols="days", by="id.key"]
+
+  return(df)
 }
 
-simulate.one <- function(){
+fit.model <- function(df){
 
-  total.samples <- sample(x=num.samples, size=length(ids), replace=TRUE)
-  traj <- mapply(make.trajectory, id=ids, n=total.samples, SIMPLIFY=T)
+  # Fit GEE model for the gap times
+  mod <- gee(gap ~ 1, id=id.key, data=df,
+             family=poisson(link="log"), corstr="exchangeable")
 
-  id.sims <- rep(ids, total.samples)
-  times <- unlist(traj)
+  # Get only the first days of sampling
+  first.day <- df[samp == 1]
 
-  new.df <- data.table(id=id.sims, days=times)
+  return(list(days=first.day$days,
+              num.samples=first.day$num.samples,
+              rate=exp(coef(mod))))
 }
 
-set.seed(10)
+simulate.study <- function(days, num.samples, rate){
 
-ps <- list()
+  # Sample the first day
+  day1 <- sample(days, size=length(days), replace=TRUE)
 
-pdf("duong-hist.pdf", width=10, height=10)
-par(mfrow=c(4, 2))
-hist(num.samples, main="DATA: Histogram of \nNumber of Samples",
-     xlab="Number of Samples Taken", xlim=c(0, 30), breaks=30,
-     col='pink')
-hist(df$days, main="DATA: Days Post \nSeroconversion Histogram",
-     xlab="Days Post Seroconversion", xlim=c(0, 1e4), breaks=seq(0, 1e4, by=50),
-     col='pink')
-p0 <- ggplot(data=df) + geom_line(aes(x=samp, y=days, group=id.key), color="#ff59c8") +
-  ggtitle("DATA: Individual Trajectories") + ylim(c(0, 10000)) +
-  ylab("Days Post Seroconversion") + xlab("Sample Number")
-ps[[1]] <- p0
+  # Sample the number of samples
+  nums <- sample(num.samples, size=length(days), replace=TRUE)
 
-for(i in 1:3){
+  # Get the rate for gap times
+  # rate <- cbind(rep(1, length(days)), day1, nums) %*% coef(mod)
 
-  sim <- simulate.one()
-  sim[, samp := 1:.N, by="id"]
+  # Get the gap-times for each
+  gaps <- lapply(nums, function(x) rpois(n=x, lambda=rate))
 
-  sim.samples <- sim[, .N, by="id"]
-  hist(sim.samples$N, main="SIM: Histogram of \nNumber of Samples",
-       xlab="Number of Samples Taken", xlim=c(0, 30), breaks=30)
-  hist(sim$days, main="SIM: Days Post \nSeroconversion Histogram",
-       xlab="Days Post Seroconversion", xlim=c(0, 1e4), breaks=seq(0, 1e4, by=50))
+  # Add on the start day
+  days.gap <- mapply(function(x, y) c(x, y), x=day1, y=gaps)
 
-  p.i <- ggplot(data=sim) + geom_line(aes(x=samp, y=days, group=id)) +
-    ggtitle("SIM: Individual Trajectories") + ylim(c(0, 10000)) +
-    ylab("Days Post Seroconversion") + xlab("Sample Number")
-  ps[[i+1]] <- p.i
+  # Calculate sampling times
+  days.tot <- lapply(days.gap, cumsum)
+
+  # Get ids and put into data frame
+  ids <- 1:length(days)
+  ids <- rep(ids, nums)
+
+  df <- data.table(id.key=ids,
+                   days=unlist(days.tot))
+  return(df)
 }
-dev.off()
 
-pdf("duong-trajectories.pdf", width=6, height=6)
-grid.arrange(grobs=ps)
-dev.off()
+simulate.studies <- function(file, nsims){
+  df <- process.study(file=file)
+  mod <- fit.model(df=df)
+  sims <- replicate(nsims, simulate.study(mod$days, mod$num.samples, mod$rate),
+                    simplify=FALSE)
+  return(sims)
+}
 
+# --------------------------------------------------------------------
+# NUMBER OF SAMPLES TAKEN PER PANEL ----------------------------------
+# --------------------------------------------------------------------
 
-
-
-
-# GAP TIMES
-df[, last.time := shift(days), by="id.key"]
-df[, gap := days - last.time]
-
-hist(df$gap)
-
-df[, num.samples := .N, by="id.key"]
-
-small <- df[num.samples < 10]
-large <- df[num.samples >= 10]
-
+df <- process.study(file=FILE)
+num.samples <- df[, lapply(.SD, max), .SDcols="samp", by="id.key"]
+samps <- num.samples$samp
+pdf("duong-hist-data.pdf", width=8, height=4)
 par(mfrow=c(1, 2))
-hist(small$gap)
-hist(large$gap)
+hist(samps, main="Histogram of Number of Samples",
+     xlab="Number of Samples Taken")
+hist(df$days, main="Days Post Seroconversion Histogram",
+     xlab="Days")
+dev.off()
 
-ggplot(data=df) + geom_jitter(aes(x=num.samples, y=gap), alpha=0.3)
+sims <- simulate.studies(file=FILE, nsims=100)
 
-mod <- gee(gap ~ 1, id=id.key, data=df,
-    family=poisson(link="log"))
-exp(mod$coefficients)
+par(mfrow=c(2, 2))
+hist(df$days)
+hist(sims[[1]]$days)
+hist(sims[[2]]$days)
+hist(sims[[3]]$days)
+
+# Fit GEE model for the gap times
+df[, samp.5 := samp >= 5]
+mod <- gee(gap ~ samp + samp.5 + samp.5*samp, id=id.key, data=df,
+           family=poisson(link="log"), corstr="exchangeable")
+
+dd <- data.table(samp=1:25)
+dd[, samp.5 := samp >= 5]
+
+int <- rep(1, 25)
+samp <- 1:25
+samp.5 <- samp >= 5
+
+dd <- cbind(int, samp, samp.5, samp*samp.5)
+preds <- exp(dd %*% coef(mod))
+dd <- data.table(dd)
+dd[, preds := preds]
+
+ggplot(data=df) + geom_line(aes(x=samp, y=gap, group=id.key)) +
+  geom_line(data=dd, aes(x=samp, y=preds), color='red')
+
+
+# TODO: Put this into the function.
+first.day <- df[samp==1]
+days <- first.day$days
+num.samples <- first.day$num.samples
+# Sample the first day
+day1 <- sample(days, size=length(days), replace=TRUE)
+
+# Sample the number of samples
+nums <- sample(num.samples, size=length(days), replace=TRUE)
+
+# Get sample numbers
+samp.num <- lapply(nums, function(x) 1:x)
+
+get.samp.dmat <- function(samp) return(cbind(1, samp, samp>=5, (samp>=5)*samp))
+
+dmats <- lapply(samp.num, get.samp.dmat)
+
+rates <- lapply(dmats, function(x) exp(x %*% coef(mod))[, 1])
+
+# Get the gap-times for each
+gaps <- lapply(rates, function(x) rpois(n=length(x), lambda=x))
+
+# Add on the start day
+days.gap <- mapply(function(x, y) c(x, y), x=day1, y=gaps)
+
+# Calculate sampling times
+days.tot <- lapply(days.gap, cumsum)
+
+# Get ids and put into data frame
+ids <- 1:length(days)
+ids <- rep(ids, nums)
+
+hist(unlist(days.tot), breaks=50)
 
 
 
+
+hist(df$days, main="Days Post Seroconversion Histogram",
+     xlab="Days", breaks=50)
