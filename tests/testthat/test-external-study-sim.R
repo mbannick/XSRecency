8447e189287b8ab38393cdb3ae3cceaff9ade6c3
Marlena Bannick <mnorwood@uw.edu>
2021-03-24 21:33:54 -0700

    bugfix for the recency indicators in phi function

--- tests/testthat/test-external-study-sim.R
+++ tests/testthat/test-external-study-sim.R
@@ -3,13 +3,13 @@ context("External study simulation")
 test_that("External study simulation", {
   set.seed(200)
   func <- function(t) 1 - pgamma(t, 1, 2)
-  expect_warning(phi <- assay.properties.nsim(1, func, bigT=2, tau=12,
-                                              integrate.FRR=FALSE))
-  expect_equal(phi$mu_est, 0.4902688, tolerance=1e-6)
-  expect_equal(phi$mu_var, 4.238794e-07, tolerance=1e-6)
-  expect_equal(phi$omega_est, 0.4793774, tolerance=1e-6)
-  expect_equal(phi$omega_var, 3.745049e-07, tolerance=1e-6)
-  expect_equal(phi$beta_est, 0.0007204611, tolerance=1e-6)
-  expect_equal(phi$beta_var, 5.186902e-07, tolerance=1e-6)
+  phi <- assay.properties.nsim(1, func, bigT=2, tau=12,
+                               integrate.FRR=FALSE)
+  expect_equal(phi$mu_est, 0.4490537, tolerance=1e-6)
+  expect_equal(phi$mu_var, 0.0003872699, tolerance=1e-6)
+  expect_equal(phi$omega_est, 0.4404954, tolerance=1e-6)
+  expect_equal(phi$omega_var, 0.0003406153, tolerance=1e-6)
+  expect_equal(phi$beta_est, 0.001440922, tolerance=1e-6)
+  expect_equal(phi$beta_var, 1.036633e-06, tolerance=1e-6)
 })
 
