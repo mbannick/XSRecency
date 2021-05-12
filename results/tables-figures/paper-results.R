1a73c1a526eac4951796492c00971305eeb99718
Marlena Bannick <mnorwood@uw.edu>
2021-05-12 07:29:29 -0700

    Adding new figure for results.

--- results/tables-figures/paper-results.R
+++ results/tables-figures/paper-results.R
@@ -234,7 +234,7 @@ dev.off()
 
 # PHI FIGURE ----------------------------------
 
-pdf('RAplot.pdf',height=6,width=10)
+pdf('RAplot-2.pdf',height=6,width=10)
 par(mfrow=c(1,2))
 
 for(i in 1:2){
@@ -262,6 +262,7 @@ for(i in 1:2){
 
   phit.const <- function(t) phit(t)*(t <= ttime) + tval*(t > ttime)
   phit.const.dnorm <- function(t) phit.const(t) + dnorm(t-7, mean=0, sd=1) / 8
+  phit.const.dnorm.ext <- function(t) phit.const(t) + pnorm(t, mean=6.5, sd=1) / 8
 
   plot(t, phit(t), col='black', type='l', ylab=expression(phi(t)), xlab=expression(t))
   abline(h=tval, lty='dashed')
@@ -272,14 +273,17 @@ for(i in 1:2){
   ##### Non-constant FRR -- peak at 7 years
   lines(t, phit.const.dnorm(t), col='blue')
 
+  ##### Non-constant FRR -- increase after 7 years
+  lines(t, phit.const.dnorm.ext(t), col='orange')
+
   if(i == 2){
     pgon <- c(seq(2, ttime, 0.01))
     pgon.phi <- 1-pgamma(pgon, shape = params[1], rate = params[2])
     polygon(x=c(pgon, rev(pgon)), y=c(rep(tval, length(pgon)), rev(pgon.phi)), col='lightgrey')
   }
 
-  legend('topright', paste0(name, c("A", "B", "C")),
-         lty=rep(1, 4),col=c("black", "red", "blue"), cex=0.75)
+  legend('topright', paste0(name, c("A", "B", "C", "D")),
+         lty=rep(1, 4),col=c("black", "red", "blue", "orange"), cex=0.75)
   abline(v=2, lty='dashed')
 }
 
