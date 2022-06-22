2f73763235eedb60efe6b72437d045ebeaacd0fa
Marlena Bannick <mnorwood@uw.edu>
2022-06-22 08:55:49 -0700

    Bugfix for parallel submission for IFS

--- results/parallel/submit-enhanced-main.sh
+++ results/parallel/submit-enhanced-main.sh
@@ -7,7 +7,7 @@ OUTPUT="/home/users/mnorwood/hutch/xs-recent/enhanced"
 
 CONSTANTS="-cwd -N recency_sim -j y -o ${OUT} -pe smp 1 -q normal.q ${SHELL} ${SCRIPT}"
 
-NSIMS=2
+NSIMS=5000
 N=5000
 P=0.29
 INC=0.032
@@ -15,13 +15,12 @@ TAU=12
 BIGT=2
 
 ARGS="-n_sims=${NSIMS} -n=${N} -p ${P} -inc ${INC} -tau ${TAU} -bigT ${BIGT} -out_dir ${OUTPUT} -last_point"
-ASSAY="-window 248 -shadow 306"
+ASSAY="-window 101 -shadow 194"
 TYPE="-itype constant"
 TAIL="-phi_tfrr 2"
 PT="-pt"
 
 BASELINE="${ARGS} ${RHO} ${TYPE} ${ASSAY} ${TAIL} ${PT}"
-OLDIFS=$IFS; IFS=','
 for GAMMA in 0 0.1
 do
   for Q in 0.2 0.4 0.6 0.8 1.0
@@ -30,10 +29,10 @@ do
     do
       for NU in 0.0 0.25
       do
-        for TRANGE in 0,4 0,2 1,3
-        do set -- $TRANGE;
+        for i in "0 4" "0 2" "1 3"
+        do 
+          set -- $i
           PTARGS="-t_min $1 -t_max $2 -q $Q -gamma $GAMMA -eta $ETA -nu $NU"
-          IFS=$OLDIFS
           echo "qsub ${CONSTANTS} ${BASELINE} ${PTARGS}"
           qsub ${CONSTANTS} ${BASELINE} ${PTARGS}
           # qsub ${CONSTANTS} ${BASELINE} ${PTARGS} -qu_int 1 -qu_slope 1
