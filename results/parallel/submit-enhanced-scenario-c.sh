60d3de9750d8cffcbb38bf0a816a9d9469b4c378
Marlena Bannick <mnorwood@uw.edu>
2022-07-27 11:23:34 -0700

    updated scenario c to have differing tmaxes

--- results/parallel/submit-enhanced-scenario-c.sh
+++ results/parallel/submit-enhanced-scenario-c.sh
@@ -9,7 +9,7 @@ mkdir ${OUTDIR}
 NSIMS=$1
 N=5000
 P=0.29
-INC=0.034
+INC=0.032
 TAU=12
 BIGT=2
 RHO=0.0039
@@ -20,14 +20,16 @@ ARGS="-n_sims=${NSIMS} -n=${N} -p ${P} -inc ${INC} -tau ${TAU} -bigT ${BIGT} -ou
 ASSAY="-window 101 -shadow 194"
 TAIL="-phi_tfrr 2"
 PT="-pt"
-TS="-t_min ${TMIN} -t_max ${TMAX}"
 
-BASELINE="${ARGS} ${ASSAY} ${TAIL} ${PT} ${TS}"
+BASELINE="${ARGS} ${ASSAY} ${TAIL} ${PT}"
 
 for Q in 0.5 1.0
 do
-    qsub ${CONSTANTS} ${BASELINE} -q $Q -itype constant
-    qsub ${CONSTANTS} ${BASELINE} -q $Q -itype constant -exclude_pt_bigT
-    qsub ${CONSTANTS} ${BASELINE} -q $Q -itype piecewise -rho ${RHO}
-    qsub ${CONSTANTS} ${BASELINE} -q $Q -itype piecewise -rho ${RHO} -exclude_pt_bigT
+    for TMAX in 4 $TAU
+    do
+        qsub ${CONSTANTS} ${BASELINE} -q $Q -itype constant -t_min $TMIN -t_max $TMAX
+        qsub ${CONSTANTS} ${BASELINE} -q $Q -itype constant -exclude_pt_bigT -t_min $TMIN -t_max $TMAX
+        qsub ${CONSTANTS} ${BASELINE} -q $Q -itype piecewise -rho ${RHO} -t_min $TMIN -t_max $TMAX
+        qsub ${CONSTANTS} ${BASELINE} -q $Q -itype piecewise -rho ${RHO} -exclude_pt_bigT -t_min $TMIN -t_max $TMAX
+    done
 done
