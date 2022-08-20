0764bf00957db40882b45346bb60dfc5b31a3790
Marlena Bannick <mnorwood@uw.edu>
2022-08-19 17:31:06 -0700

    updates for table 1 c, the misspecification scenario for constant-linear incidence.

--- results/tables-figures/table-1-enhanced-c.R
+++ results/tables-figures/table-1-enhanced-c.R
@@ -17,16 +17,17 @@ source("~/repos/XSRecency/R/data-generator.R")
 
 # READ IN VERSIONED RESULTS ---------------------------------
 
-version <- "~/Documents/FileZilla/xs-recent/enhanced/27-07-2022-08-33-51"
+version <- "~/Documents/FileZilla/xs-recent/enhanced/27-07-2022-21-53-56/"
 summ <- fread(paste0(version , "/summary.csv"))
 
-summ[, tname := ifelse(itype == "constant", "Constant", "Constant-Linear")]
+summ[, tname := ifelse(itype == "constant", "Constant", "Piecewise")]
 summ[, pname := "Constant"]
 summ[, sname := "1B"]
-summ[exclude_pt_bigT == TRUE, estype := "exlude non-recent"]
-summ[exclude_pt_bigT == FALSE, estype := "include non-recent"]
+summ[exclude_pt_bigT == TRUE, estype := "only recent"]
+summ[exclude_pt_bigT == FALSE, estype := "all tests"]
+summ[, estype := factor(estype, levels=c("all tests", "only recent"))]
 
-summ <- summ[, .(q, tname, estype, q_eff, estimator_type, assay_vals, bias, se, mse)]
+summ <- summ[, .(q, tname, t_max, estype, q_eff, estimator_type, assay_vals, bias, se, mse)]
 summ[, bias := bias * 100]
 summ[, se := se * 100]
 summ[, mse := mse * 100]
@@ -37,23 +38,23 @@ summ[, mse := lapply(mse, function(x) sprintf("%.3f", x))]
 est <- summ[assay_vals == "est"]
 true <- summ[assay_vals == "true"]
 
-est.bias <- dcast(est, q + tname + estype + q_eff ~ estimator_type, value.var=c("bias"))
-true.bias <- dcast(true, q + tname + estype + q_eff ~ estimator_type, value.var=c("bias"))
+est.bias <- dcast(est, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("bias"))
+true.bias <- dcast(true, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("bias"))
 
-est.se <- dcast(est, q + tname + estype + q_eff ~ estimator_type, value.var=c("se"))
-true.se <- dcast(true, q + tname + estype + q_eff ~ estimator_type, value.var=c("se"))
+est.se <- dcast(est, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("se"))
+true.se <- dcast(true, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("se"))
 
-est.mse <- dcast(est, q + tname + estype + q_eff ~ estimator_type, value.var=c("mse"))
-true.mse <- dcast(true, q + tname + estype + q_eff ~ estimator_type, value.var=c("mse"))
+est.mse <- dcast(est, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("mse"))
+true.mse <- dcast(true, q + tname + t_max + estype + q_eff ~ estimator_type, value.var=c("mse"))
 
-EST <- cbind(est.bias, est.se[, -c(1:4)], est.mse[, -c(1:4)]) %>% data.table
-TRUTH <- cbind(true.bias, true.se[, -c(1:4)], true.mse[, -c(1:4)]) %>% data.table
+EST <- cbind(est.bias, est.se[, -c(1:5)], est.mse[, -c(1:5)]) %>% data.table
+TRUTH <- cbind(true.bias, true.se[, -c(1:5)], true.mse[, -c(1:5)]) %>% data.table
 
 addtorow <- list()
-addtorow$pos <- seq(2, nrow(TRUTH), by=2) %>% as.list
+addtorow$pos <- seq(4, nrow(TRUTH), by=4) %>% as.list
 addtorow$command <- rep("\\hline \n", length(addtorow$pos))
 
-tab <- xtable(TRUTH, align=rep("c", 11), digits=2)
+tab <- xtable(TRUTH, align=rep("c", 12), digits=2)
 print(tab, include.rownames=FALSE,
       add.to.row = addtorow)
 
